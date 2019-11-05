0a8f2b447f0f06c2046d706ddbddb69e46c47668
Henry Chung <chungthehao@gmail.com>
2019-11-05 20:54:21 +0700

    Displaying current user's post - Building User Interface

--- resources/assets/js/pages/MyPostsPage.vue
+++ resources/assets/js/pages/MyPostsPage.vue
@@ -1,3 +1,102 @@
 <template>
-    <h1>Curent user's posts</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">All</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Questions</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Answers</a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="post-badge accepted">Q</span>
+                                        <span class="ml-4 votes-count accepted">10</span>
+                                    </div>
+
+                                    <div class="col-md-9">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+
+                                    <div class="col text-right">Jun 6'15</div>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="post-badge accepted">A</span>
+                                        <span class="ml-4 votes-count accepted">36</span>
+                                    </div>
+
+                                    <div class="col-md-9">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+
+                                    <div class="col text-right">Mar 9'15</div>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="post-badge accepted">Q</span>
+                                        <span class="ml-4 votes-count accepted">10</span>
+                                    </div>
+
+                                    <div class="col-md-9">
+                                        Lorem ipsum dolor sit amet
+                                    </div>
+
+                                    <div class="col text-right">Nov 7'20</div>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+    $color-green: rgba(95, 187, 126, 1);
+
+    .votes-count {
+        border: 1px solid #ddd;
+        display: inline-block;
+        min-width: 40px; // Số lớn còn nở ra tiếp đc
+        text-align: center;
+
+        &.accepted {
+            background: $color-green;
+            border-color: $color-green;
+            color: white;
+        }
+    }
+
+    .post-badge {
+        border: 1px solid #ddd;
+        display: inline-block;
+        width: 25px;
+        text-align: center;
+        border-radius: 100%;
+
+        &.accepted {
+            border-color: $color-green;
+            color: $color-green;
+        }
+    }
+</style>
